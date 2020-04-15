ffa9a4d0811f5fb0a17c4793f7a5b465067de543
Nick <nick@npsoftware.co.uk>
2020-04-15 11:52:52 +0100

    Added crappy splash screen and background music.
    Also added a plane for the player.

---
+++ Assets/DelayStarter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DelayStarter : MonoBehaviour
+{
+    [SerializeField] int SceneIndexToLoad;
+    [SerializeField] float Delay;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        Invoke(nameof(LoadFirstScene), Delay);
+    }
+
+    private void LoadFirstScene()
+    {     
+        SceneManager.LoadScene(SceneIndexToLoad);
+    }
+}
